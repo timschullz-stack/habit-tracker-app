0a4662d26217e159837092799a091b49bb4338d6
timschullz-stack <timschullz@gmail.com>
2026-07-25 12:19:12 +0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.summerhabits
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.summerhabits.ui.screens.MainApp
+import com.example.summerhabits.ui.theme.SummerHabitsTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SummerHabitsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainApp()
+                }
+            }
+        }
+    }
+}
