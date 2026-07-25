9a7dfebe3be0262df42e701f6b63bec218f8cc7e
timschullz-stack <timschullz@gmail.com>
2026-07-25 15:07:52 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,1 @@
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version"1.9.20" apply false
-}
+
