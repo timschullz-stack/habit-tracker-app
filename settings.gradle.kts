891b20d98166b1789f6a5f35f6a743d2e89f0e67
timschullz-stack <timschullz@gmail.com>
2026-07-25 14:26:38 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 PluginManagment {
-   repositories {
-     google()
-     mavenCentral()
+    repositories {
+        google()
+        mavenCentral()
      gradlePluginPortal()
-   }
+    }
 }
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-  }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 rootProject.name = "HabitTracker"
